b7aa9d9abda223c6c3dd51a6e4a02de471d57c55
Aleksej222 <aleksejsuzic2001@gmail.com>
2023-09-09 22:53:17 +0200

    On project click tasks appear in the DOM. Full of bugs.

--- src/index.js
+++ src/index.js
@@ -33,16 +33,16 @@ listOptions.forEach(option => {
 });
 
 // !! Test part
-let selectedProject = allProjects[0];
+// let selectedProject = allProjects[0];
 
-let testTask = new Task('First task', 'This is first task created that belongs to the project.', '07.09.2023','low');
-selectedProject.tasks.push(testTask);
-let anotherTask = new Task('Random task', 'This task belongs to the same project.', '07.09.2023','medium');
-selectedProject.tasks.push(anotherTask);
+// let testTask = new Task('First task', 'This is first task created that belongs to the project.', '07.09.2023','low');
+// selectedProject.tasks.push(testTask);
+// let anotherTask = new Task('Random task', 'This task belongs to the same project.', '07.09.2023','medium');
+// selectedProject.tasks.push(anotherTask);
 
-selectedProject = allProjects[1];
-let anotherProjectTask = new Task('Different project', 'This task belongs to the different project.', '07.09.2023','medium');
-selectedProject.tasks.push(anotherProjectTask);
+// selectedProject = allProjects[1];
+// let anotherProjectTask = new Task('Different project', 'This task belongs to the different project.', '07.09.2023','medium');
+// selectedProject.tasks.push(anotherProjectTask);
 // !! End test part
 
 let buttonAddProject = document.querySelector('.btn-addProject');
@@ -50,17 +50,17 @@ buttonAddProject.addEventListener('click', addNewProject);
 
 // let tasksContainer = document.querySelector('.tasks-container');
 
-let btnTestFnc = document.querySelector('.edit-task');
+// let btnTestFnc = document.querySelector('.edit-task');
 
-btnTestFnc.addEventListener('click', testThis);
+// btnTestFnc.addEventListener('click', testThis);
 
-function testThis() {
-    // console.log(allProjects);
+// function testThis() {
+//     // console.log(allProjects);
 
-    console.log(selectedProject);
+//     console.log(selectedProject);
 
     
-}
+// }
 
 // ** Lepo rasporedit kod po fajlovima, za dodavanje projekta u jedan, za brisanje u drugi, za edit u treci (primjer)
 // ?? Di ide kod za prve tri opcije (all tasks....), kako to izvest
--- src/showCorrectProject.js
+++ src/showCorrectProject.js
@@ -1,6 +1,6 @@
 import { allProjects } from ".";
 
-// Set allProjects array as an empty array if it doesn't exist in local storage
+// ** Set allProjects array as an empty array if it doesn't exist in local storage
 if (!allProjects) {
 
     allProjects = [];
@@ -14,31 +14,27 @@ export function showCorrectProject() {
 
     
     let listOptionSelected = this.id;
-    let selectedProject;
+    let selectedProjectTasks;
 
     switch (listOptionSelected) {
-        case 'all-tasks':
-            selectedProject = showAllTasks();
+        case 'All tasks':
+            selectedProjectTasks = showAllTasks();
             break;
 
-        case 'today-tasks':
-            selectedProject = showTodayTasks();
+        case 'Today':
+            selectedProjectTasks = showTodayTasks();
             break;
     
-        case 'this-week-tasks':
-            selectedProject = showThisWeekTasks();
+        case 'This week':
+            selectedProjectTasks = showThisWeekTasks();
             break;
 
         default:
-            selectedProject = otherProjectTasks(listOptionSelected);
+            selectedProjectTasks = otherProjectTasks(listOptionSelected);
             break;
     }
     
-
-    // console.log(selectedProject);
-
-    displayProjectTasks(selectedProject);
-
+    displayProjectTasks(listOptionSelected, selectedProjectTasks);
 }
 
 // ** Show tasks from every project
@@ -46,19 +42,17 @@ function showAllTasks() {
 
     let allTasksArr = [];
 
-    // Proc kroz sve elemente u listi (projekte) i pushat sve zadatke u glavni array koji ce prikazat te zadatke
+    // Go through every element in the list (projects) and push all the tasks into the main array that will show those tasks
     allProjects.forEach(project => {
 
         project.tasks.forEach(task => {
             
-            console.log(task);
             allTasksArr.push(task);
 
         });
     });
 
     return allTasksArr;  
-
 }
 
 // ** Show tasks with today's date
@@ -74,19 +68,73 @@ function showThisWeekTasks() {
 // ** Show tasks from project that is not in the main list (all-tasks...)
 function otherProjectTasks(listOptionSelected) {
 
-    let selectedProject = allProjects.find(project => {
+    let selectedProjectTasks = allProjects.find(project => {
         return project.title == listOptionSelected;
     });
 
-    return selectedProject;
-
+    return selectedProjectTasks.tasks;
 }
 
 // ** Display selected project tasks in the DOM
-function displayProjectTasks(selectedProject) {
+function displayProjectTasks(listOptionSelected, selectedProjectTasks) {
 
-    // console.log(selectedProject);
+    // console.log(selectedProjectTasks);
 
-    // console.log(listOptions);
+    let tasksContainer = document.querySelector('.tasks-container');
+    let projectName = tasksContainer.querySelector('.project-name');
+    let tasksList = tasksContainer.querySelector('.tasks-list');
+
+    projectName.innerText = listOptionSelected;
+
+    selectedProjectTasks.forEach(task => {
+        
+        tasksList.appendChild(createTaskHTML(task));
+
+    });
+
 
 }
+
+function createTaskHTML(task) {
+
+    let taskContainer = document.createElement('div');
+    taskContainer.classList.add('task');
+
+    let taskDate = document.createElement('span');
+    taskDate.classList.add('task-date');
+    taskDate.innerText = task.date;
+
+    let taskTitle = document.createElement('h4');
+    taskTitle.classList.add('task-title');
+    taskTitle.innerText = task.title;
+
+    let taskDescription = document.createElement('span');
+    taskDescription.classList.add('task-description');
+    taskDescription.innerText = task.description;
+
+    let taskBtns = document.createElement('div');
+    taskBtns.classList.add('tasks-btns');
+
+    taskContainer.appendChild(taskDate);
+    taskContainer.appendChild(taskTitle);
+    taskContainer.appendChild(taskDescription);
+    taskContainer.appendChild(taskBtns);
+
+    return taskContainer;
+
+    // !! Color changes based on priority of the task
+}   
+
+/* 
+<div class="task">
+<span class="task-date">22/08/23</span>
+<h4 class="task-title">Study math</h4>
+<span class="task-description">Go through all the tasks in the book.</span>
+<div class="task-btns">
+    <button class="edit-task">pen</button>
+    <button class="delete-task">trash-can</button>
+</div>
+</div>
+
+*/
+// !! Bug ne prikaze title novo dodanog projekta
