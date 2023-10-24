d31b1f26b07efaa25d4d7c4dd085e47400721d56
Aleksej222 <aleksejsuzic2001@gmail.com>
2023-10-24 22:47:35 +0200

    Worked on task deletion.

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -1,5 +1,32 @@
-export function deleteTask(task) {
+import { allProjects } from ".";
+
+export function deleteTask(taskDeleted) {
     
-    console.log(task);
+    // console.log(task);
+    // console.log(allProjects);
+
+    allProjects.forEach(project => {
+
+        // console.log(project);
+
+        project.tasks.forEach(task => {
+
+            if (task.id == taskDeleted.id) {
+                console.log(taskDeleted);
+
+                let index = project.tasks.indexOf(taskDeleted);
+
+                console.log(index);
+
+            }
+            // console.log(task == taskDeleted);
+
+        });
+
+    });
 
 }
+
+
+// ?? Kako dobit pravilni index zadatka, tj. kako otrkit koji je pravi, i kako dobit poziciju.
+// ?? Kako izbrisat zadatak iz arraya
