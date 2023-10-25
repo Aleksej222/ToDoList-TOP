017899fc18ed37019382a22af01914f2afea1c5c
Aleksej222 <aleksejsuzic2001@gmail.com>
2023-10-25 22:04:37 +0200

    Find and delete correct task done. Need to update localStorage and html.

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -1,8 +1,8 @@
 import { allProjects } from ".";
 
+// ** Function that deletes task on button click
 export function deleteTask(taskDeleted) {
-    
-    // console.log(task);
+
     // console.log(allProjects);
 
     allProjects.forEach(project => {
@@ -12,21 +12,50 @@ export function deleteTask(taskDeleted) {
         project.tasks.forEach(task => {
 
             if (task.id == taskDeleted.id) {
-                console.log(taskDeleted);
+                // console.log(taskDeleted);
 
                 let index = project.tasks.indexOf(taskDeleted);
 
-                console.log(index);
+                // console.log(index);
 
             }
             // console.log(task == taskDeleted);
+            // console.log(task);
 
         });
 
     });
 
+    for (let i = 0; i < allProjects.length; i++) {
+
+        let projectTasks = allProjects[i].tasks;
+        // console.log(projectTasks);
+
+        for (let j = 0; j < projectTasks.length; j++) {
+
+            let task = projectTasks[j];
+            // console.log(task);
+
+            // Zadaci bez id daju gresku (tj sve ih pokupi), treba ih se svejedno izbrisat
+            if (taskDeleted.id == task.id) {
+
+                // console.log(task);
+                // console.log(taskDeleted);
+
+                // console.log(i, j);
+                // console.log(projectTasks[j]);
+                // console.log(allProjects[i].tasks[j]);
+
+                allProjects[i].tasks.splice(0,1);  // Delete the correct task
+
+                console.log(allProjects[i]);
+
+            }
+
+        }
+
+    }
+
 }
 
-
-// ?? Kako dobit pravilni index zadatka, tj. kako otrkit koji je pravi, i kako dobit poziciju.
-// ?? Kako izbrisat zadatak iz arraya
+// !! Kako uklonit html zadatka
