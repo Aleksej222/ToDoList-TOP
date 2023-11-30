9a4b6c146f0fff4b8367cea1b1080b9c803ccbcc
Aleksej222 <aleksejsuzic2001@gmail.com>
2023-11-30 21:21:11 +0100

    Project delete wip

--- src/deleteProject.js
+++ src/deleteProject.js
@@ -7,9 +7,48 @@ export function deleteProject() {
 
     // console.log('test');
 
+    // let deleteBool = openConfirmationModal();
+    openConfirmationModal();
+
+}
+
+// ** Create html for confirmation modal window
+function openConfirmationModal() {
+
+    let taskContainer = document.querySelector('.tasks-container');
+
+    let confirmationContainer = document.createElement('div');
+    confirmationContainer.classList.add('confirmation-container');
+    
+    let messageText = document.createElement('span');
+    messageText.textContent = 'Are you sure that you want to delete the whole project?'; 
+
+    let btnConfirm = document.createElement('button');
+    btnConfirm.textContent = 'Yes';
+
+    let btnDeny = document.createElement('button');
+    btnDeny.textContent = 'No';
+
+    confirmationContainer.appendChild(messageText);
+    confirmationContainer.appendChild(btnConfirm);
+    confirmationContainer.appendChild(btnDeny);
+
+    taskContainer.appendChild(confirmationContainer);
+
+}
+
+// ** Function that deletes the selected project
+function deleteProjectConfirmed() {
+
+}
+
+// ** Cancel delete of project, delete modal window html
+function cancelDelete() {
+
 }
 
 
+// TODO: Dodat stajling za confirmation modal
 
 // TODO: Postavit confirmation modal window, prije brisanja, sa upozorenjem
 // TODO: Napravit refresh kad se obrise cijeli projekt (najlakse)
