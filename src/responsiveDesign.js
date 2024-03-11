d3e2e94ea1ee50944db1460758b3210f76201eaa
Aleksej222 <aleksejsuzic2001@gmail.com>
2024-03-11 22:12:58 +0100

    Burger menu works, switches content on click. Needs styling.

--- src/responsiveDesign.js
+++ src/responsiveDesign.js
@@ -2,6 +2,26 @@
 // ** Hide tasks content and set menu content visible when clicked on hamburger menu
 export function setMenuContentVisibility() {
 
-    console.log('test');
 
+    let tasksContainer = document.querySelector('.tasks-container');
+    let menuContainer = document.querySelector('.menu-container');
+    
+    let burgerContainer = document.querySelector('.burger-container');
+
+    if (!menuContainer.style.display || menuContainer.style.display == 'none') {
+
+        menuContainer.style.display = 'block';
+        tasksContainer.style.display = 'none';
+
+        burgerContainer.style.backgroundColor = '#cde9ff';
+
+
+    } else {
+
+        menuContainer.style.display = 'none';
+        tasksContainer.style.display = 'block';
+
+        burgerContainer.style.backgroundColor = 'white';
+
+    }
 }
